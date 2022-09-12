f74df01e7aa4d2e796cc9b57b9983bd1ab327eb4
matthewrmcevoy <matthewrmcevoy@gmail.com>
2022-09-11 21:28:01 -0400

    testing download functionality still

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,6 +3,8 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import kotlin.properties.Delegates
@@ -12,12 +14,38 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
-
-    private val valueAnimator = ValueAnimator()
-
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+    private val painter = Paint().apply{
+        isAntiAlias = true
+        textSize = resources.getDimension(R.dimen.default_text_size)
     }
+    var buttonText = "DOWNLOAD"
+    private var dwnldProg = 0
+    private var valueAnimator = ValueAnimator()
+
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        when(new){
+            ButtonState.Loading->{
+                buttonText = "DOWNLOADING"
+            valueAnimator = ValueAnimator.ofInt(0,widthSize).apply{
+                duration=700
+                addUpdateListener { valueAnimator ->
+                    dwnldProg = animatedValue as Int
+                    valueAnimator.repeatCount = ValueAnimator.INFINITE
+                    valueAnimator.repeatMode = ValueAnimator.REVERSE
+                    invalidate()
+                }
+                start()
+            }
+            }
+            ButtonState.Completed -> {
+                buttonText = "DOWNLOADED"
+                valueAnimator.end()
+                dwnldProg=100
+                invalidate()
+            }
+        }
+    }
+    var buttonEnabled = false
 
 
     init {
@@ -25,22 +53,38 @@ class LoadingButton @JvmOverloads constructor(
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.drawColor(resources.getColor(R.color.colorPrimary))
+
+        painter.color = resources.getColor(R.color.colorPrimaryDark)
+        widthSize = width
+        heightSize = height
+        canvas.drawRect(0f,0f,(widthSize.toFloat()*dwnldProg/100), heightSize.toFloat(), painter)
+
+        when(buttonEnabled){
+            true -> painter.color = Color.WHITE
+            false -> painter.color = Color.GRAY
+        }
+        painter.textAlign = Paint.Align.CENTER
+
+        val xPos = (width/2).toFloat()
+        val yPos = ((height /2)-((painter.descent()+painter.ascent())/2)).toFloat()
+        canvas.drawText(buttonText,xPos,yPos,painter)
 
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
-        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
-        val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
-        )
-        widthSize = w
-        heightSize = h
-        setMeasuredDimension(w, h)
-    }
+//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+//        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
+//        val h: Int = resolveSizeAndState(
+//            MeasureSpec.getSize(w),
+//            heightMeasureSpec,
+//            0
+//        )
+//        widthSize = w
+//        heightSize = h
+//        setMeasuredDimension(w, h)
+//    }
 
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,10 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +22,7 @@ import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var URL = "NA"
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -31,19 +36,56 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            if(URL=="NA"){
+                Toast.makeText(this,"You must select an item first!", Toast.LENGTH_SHORT).show()
+            }else {
+                download()
+                custom_button.buttonState = ButtonState.Loading
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if(downloadID==id){
+                Log.i("FragmentActivity","Download Completed")
+                custom_button.buttonState = ButtonState.Completed
+            }
+        }
+    }
+
+    fun onRadioSelected(view: View){
+        if(view is RadioButton){
+            val checked = view.isChecked
+            when(view.getId()){
+                R.id.glide_radio_bttn -> if(checked){
+                    Log.i("Frag","Glide Bttn Checked")
+                    custom_button.buttonEnabled = true
+                    custom_button.invalidate()
+                    URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+                }
+                R.id.curRepo_radio_bttn -> if(checked){
+                    Log.i("Frag","Current Repo Bttn Checked")
+                    custom_button.buttonEnabled = true
+                    custom_button.invalidate()
+                    URL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+                }
+                R.id.retro_radio_bttn -> if(checked){
+                    Log.i("Frag","Retro Bttn Checked")
+                    custom_button.buttonEnabled = true
+                    custom_button.invalidate()
+                    URL = "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+                }
+            }
         }
     }
 
     private fun download() {
+        Log.i("FragmentActivity","Download called")
         val request =
             DownloadManager.Request(Uri.parse(URL))
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -56,8 +98,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+//        private const val URL =
+//            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
